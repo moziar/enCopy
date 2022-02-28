a6d271bcf75e167153d1a3852dc43b1180fd3989
Moz <35004025+moziar@users.noreply.github.com>
2022-02-28 20:52:19 +0800

    upLoad js script

---
+++ enCopy.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         解除网页复制限制
+// @icon         https://static.zhihu.com/heifetz/favicon.ico
+// @version      0.1
+// @description  用来解除复制网页的限制，已知知乎盐选可用
+// @author       moziar
+// @match        *://www.zhihu.com/*
+// @icon         https://www.google.com/s2/favicons?domain=bing.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    document.body.contentEditable=true
+})();
